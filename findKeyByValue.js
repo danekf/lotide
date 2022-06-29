0fcff33f5e46d68873f696b9bd09b8d751a2065b
Danek <danek.fill@outlook.com>
2022-06-29 18:18:18 +0000

    added findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !=== ${expected}`);
+  }
+
+};
+const findKeyByValue = function (tvShows, key){
+  let result = '';
+  for (let genre in tvShows) {
+    if(tvShows[genre] === key){
+      result = genre;
+     }
+  }
+
+  return result;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
