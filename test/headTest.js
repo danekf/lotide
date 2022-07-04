e04e8ff4fe55edd9ab74ef617f805530252d8d0a
Danek <danek.fill@outlook.com>
2022-07-04 17:20:13 +0000

    changed to incorporate mocha.chai funcitonality

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,23 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+// const head = require('../head');
+// const assertEqual = require('../assertEqual');
 
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// // TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// ^^^^^^Old test, for reference
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  
+});
